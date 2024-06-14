1a4cdea97060c98e8aea19235aed8bb536368eff
Vitalina Malynovska <vitalinamalinovskaya557@gmail.com>
2024-06-14 15:49:46 +0200

    Created urls (routs) for user & added to urls.py in main project

--- library_service/urls.py
+++ library_service/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/user/", include("user.urls"), name="user"),
 ]
 
 if not settings.TESTING:
