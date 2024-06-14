6497d21cb9a94571fde1aacb5fae9c151e9c7c53
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-06-14 21:16:31 +0300

    Add Borrowing model and migrations to borrowing app

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+from book.models import Book
+
+User = get_user_model()
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField(auto_now_add=True)
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(null=True, blank=True)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.user} borrowed {self.book} ({self.borrow_date})"
