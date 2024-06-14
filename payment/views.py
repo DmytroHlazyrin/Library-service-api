2c12c214b0084d806d62478a0f6aa2e2b469e588
Vitalina Malynovska <vitalinamalinovskaya557@gmail.com>
2024-06-14 18:46:25 +0200

    created view for list & details Payment

--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from payment.models import Payment
+from payment.permissions import IsAdminOrOwner
+from payment.serializers import PaymentSerializer, PaymentListSerializer
+
+
+class PaymentListView(generics.ListAPIView):
+    serializer_class = PaymentListSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return Payment.objects.all()
+        return Payment.objects.filter(user=user)
+
+
+class PaymentDetailView(generics.RetrieveAPIView):
+    serializer_class = PaymentSerializer
+    permission_classes = (IsAuthenticated, IsAdminOrOwner)
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return Payment.objects.all()
+        return Payment.objects.filter(user=user)
