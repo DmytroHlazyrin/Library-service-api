a41c6548536032ecd7800f26f894a3c88870e57b
Vitalina Malynovska <vitalinamalinovskaya557@gmail.com>
2024-06-15 13:03:47 +0200

    changed filtering in get_queryset

--- payment/views.py
+++ payment/views.py
@@ -8,13 +8,13 @@ from payment.serializers import PaymentSerializer, PaymentListSerializer
 
 class PaymentListView(generics.ListAPIView):
     serializer_class = PaymentListSerializer
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, IsAdminOrOwner)
 
     def get_queryset(self):
         user = self.request.user
         if user.is_staff:
             return Payment.objects.all()
-        return Payment.objects.filter(user=user)
+        return Payment.objects.filter(borrowing_id__user=user)
 
 
 class PaymentDetailView(generics.RetrieveAPIView):
@@ -25,4 +25,4 @@ class PaymentDetailView(generics.RetrieveAPIView):
         user = self.request.user
         if user.is_staff:
             return Payment.objects.all()
-        return Payment.objects.filter(user=user)
+        return Payment.objects.filter(borrowing_id__user=user)
