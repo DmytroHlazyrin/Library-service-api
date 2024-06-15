f0430435a77f200a816e4028d7f95196256b0694
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-06-15 12:39:44 +0300

    Update Borrowing model and add migrations
    Add 'related_name' to book and user fields

--- borrowing/models.py
+++ borrowing/models.py
@@ -10,8 +10,16 @@ class Borrowing(models.Model):
     borrow_date = models.DateField(auto_now_add=True)
     expected_return_date = models.DateField()
     actual_return_date = models.DateField(null=True, blank=True)
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        related_name="borrowing"
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="borrowing"
+    )
 
     def __str__(self):
         return f"{self.user} borrowed {self.book} ({self.borrow_date})"
