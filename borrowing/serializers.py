fb441fe31b48b89894e33bef90a0281ac33c180a
Vitalina Malynovska <vitalinamalinovskaya557@gmail.com>
2024-06-16 00:20:44 +0200

    ...

--- borrowing/serializers.py
+++ borrowing/serializers.py
@@ -4,17 +4,17 @@ from borrowing.models import Borrowing
 
 
 class BorrowingSerializer(serializers.ModelSerializer):
-    book = serializers.StringRelatedField()
-    user_email = serializers.CharField(source='user.email', read_only=True)
+    # book = serializers.StringRelatedField()
+    # user_email = serializers.CharField(source='user.email', read_only=True)
 
     class Meta:
         model = Borrowing
         fields = (
             "id",
             "book",
-            "user_email",
+            "user",
             "borrow_date",
             "expected_return_date",
             "actual_return_date",
         )
-        read_only_fields = ("actual_return_date", "borrow_date")
+        # read_only_fields = ("actual_return_date", "borrow_date")
