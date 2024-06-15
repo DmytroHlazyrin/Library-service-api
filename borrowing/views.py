fef94c5ec95a717b8a10776909c5bbd03a4816e5
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-06-15 13:26:05 +0300

    Update 'get_queryset' in BorrowingListCreateAPIView
    Add constraint for filtering by user_id, just admin can see all users

--- borrowing/views.py
+++ borrowing/views.py
@@ -17,12 +17,17 @@ class BorrowingListCreateAPIView(generics.ListCreateAPIView):
     permission_classes = (IsAuthenticated, IsAdminOrOwner)
 
     def get_queryset(self):
-        queryset = super().get_queryset()
-        user_id = self.request.query_params.get("user_id")
-        is_active = self.request.query_params.get("is_active")
+        user = self.request.user
 
-        if user_id:
-            queryset = queryset.filter(user=user_id)
+        if user.is_staff:
+            queryset = super().get_queryset()
+            user_id = self.request.query_params.get("user_id")
+            if user_id:
+                queryset = queryset.filter(user=user_id)
+        else:
+            queryset = super().get_queryset().filter(user=user)
+
+        is_active = self.request.query_params.get("is_active")
 
         if is_active is not None:
             is_active = is_active.lower() in ("true", "1")
