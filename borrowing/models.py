c512aa526431dbb735cb8ee8f15704d6bc659bcf
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-06-15 08:49:48 +0300

    Update Borrowing model and add migration
    Remove 'is_active' field

--- borrowing/models.py
+++ borrowing/models.py
@@ -12,7 +12,6 @@ class Borrowing(models.Model):
     actual_return_date = models.DateField(null=True, blank=True)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return f"{self.user} borrowed {self.book} ({self.borrow_date})"
