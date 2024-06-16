e185e3908d929146a1cb0558e2b1ea3455e8ff67
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-06-16 19:05:04 +0300

    Add borrowing detail serializer

--- borrowing/serializers.py
+++ borrowing/serializers.py
@@ -1,10 +1,13 @@
 from rest_framework import serializers
 
+from book.serializers import BookSerializer
 from borrowing.models import Borrowing
+from payment.serializers import PaymentSerializer
+from user.serializers import UserSerializer
 
 
 class BorrowingSerializer(serializers.ModelSerializer):
-    # book = serializers.StringRelatedField()
+    book = serializers.StringRelatedField()
     user_email = serializers.CharField(source="user.email", read_only=True)
 
     class Meta:
@@ -23,3 +26,21 @@ class BorrowingCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Borrowing
         fields = ("id", "book", "expected_return_date")
+
+
+class BorrowingDetailSerializer(BorrowingSerializer):
+    book = BookSerializer()
+    user = UserSerializer()
+    payments = PaymentSerializer(many=True)
+
+    class Meta:
+        model = Borrowing
+        fields = (
+            "id",
+            "book",
+            "user",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+            "payments"
+        )
