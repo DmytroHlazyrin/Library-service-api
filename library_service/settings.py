b0bc608bf79b36ccd8dd1e4a68176394b9e06502
Vitalina Malynovska <vitalinamalinovskaya557@gmail.com>
2024-06-16 11:05:41 +0200

    added stripe publ. & sicret keys

--- library_service/settings.py
+++ library_service/settings.py
@@ -48,9 +48,9 @@ MIDDLEWARE = [
 ]
 
 REST_FRAMEWORK = {
-    # "DEFAULT_AUTHENTICATION_CLASSES": (
-    #     "rest_framework_simplejwt.authentication.JWTAuthentication",
-    # ),
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
 }
 
 SIMPLE_JWT = {
