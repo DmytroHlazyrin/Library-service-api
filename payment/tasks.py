7920d9777568487907a6706d6b0e113127cbf4dc
Vitalina Malynovska <vitalinamalinovskaya557@gmail.com>
2024-06-16 19:28:29 +0200

    fixed flake

--- payment/tasks.py
+++ payment/tasks.py
@@ -3,12 +3,12 @@
 from django.utils import timezone
 from .models import Payment
 
+
 # @shared_task
 def check_stripe_sessions():
     now = timezone.now()
     expired_payments = Payment.objects.filter(
-        status=Payment.PaymentStatus.PENDING,
-        session_expiry__lt=now
+        status=Payment.PaymentStatus.PENDING, session_expiry__lt=now
     )
 
     for payment in expired_payments:
